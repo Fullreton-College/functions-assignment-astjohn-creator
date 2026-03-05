4edd793f2c0a54a4448b53a2d5f4e35c8affb11d
St John, Austin <pokeboy7to8@gmail.com>
2026-03-05 14:34:42 -0800

    Finished

--- Function_4.cpp
+++ Function_4.cpp
@@ -2,12 +2,35 @@
 using namespace std;
 
 //define necessary functions
+void computeCoins(int value, int& num, int& remainder){
+  num = remainder / value;
+  remainder = remainder % value;
+}
 int main()
 {
+  char choice;
 
-  //your code goes here
+  do{
+    int amount;
+    int quarters, dimes, pennies;
+    cout << "Enter change( 1 to 99): ";
+    cin >> amount;
+
+    int origin = amount;
+    computeCoins(25, quarters, amount);
+    computeCoins(10, dimes, amount);
+    pennies = amount;
+    
+    cout << origin << " cents could be "<< quarters<< " quarter(s) " << dimes << " dime(s) and " << pennies << " penny(pennies) "<< endl;
+    
+    cout << "Enter Y or y to continue, any other halis ";
+    cin >> choice;
+    cout<< endl;
+
+  }
+  while(choice == 'Y'|| choice == 'y');
+  
 
 return 0;
 }
 
-//implement functions
