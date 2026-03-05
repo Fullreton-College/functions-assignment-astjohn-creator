76b381d3d23b8866ae9190f2c5ac14b3911aa8e3
St John, Austin <pokeboy7to8@gmail.com>
2026-03-05 14:32:36 -0800

    Finished

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,13 +3,32 @@
 using namespace std;
 
 /* Define your function here */ 
+string CoinFlip(){
+   int randomValue = rand() % 2;
+   if (randomValue == 1) {
+      return "Heads";
+   }
+   else{
+      return "Tails";
+   }
+}
 
 int main() {
    // Add more variables as needed
+   int numFlips;
+   int seedValue;
+   cin >> seedValue;
    
-   srand(2);  // Unique seed
+   
+   srand(seedValue);  // Unique seed
+   cin >> numFlips;
 
    /* Type your code here */
+   for (int i = 0; i < numFlips; ++i){
+      cout << CoinFlip()<< " ";
+   }
+
+   cout << endl;
 
    return 0;
 }
